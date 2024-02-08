a143b44bee133d3a69f2cd202ee1992a452762a6
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2024-02-08 15:50:18 +0300

    update

---
+++ Presentation/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponent/_TestimonialComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponent;
+
+namespace UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponent
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7251/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<string>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
