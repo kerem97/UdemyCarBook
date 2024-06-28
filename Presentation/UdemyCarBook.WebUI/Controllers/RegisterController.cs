7c244718b2d69f06e33561773897476ae00f0e90
Kerem DURAK <kerem.durak@yahoo.com>
2024-06-28 13:29:44 +0300

    updatedd & finished

---
+++ Presentation/UdemyCarBook.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text;
+using Newtonsoft.Json;
+using UdemyCarBook.Application.DTOs.Register;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult CreateAppUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAppUser(CreateRegisterDto createRegisterDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createRegisterDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7251/api/Registers", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
